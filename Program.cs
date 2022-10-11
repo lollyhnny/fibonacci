992c10a54b77769fa1e23b78d2cec8e5a3cd0e7e
slims <slims@LAPTOP-0I6JMJBR>
2022-10-11 16:14:31 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program1
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen sayaç değeri giriniz: ");
+        int sayac = Convert.ToInt32(Console.ReadLine());
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        Console.Write("Fİbonacci dizisi: " + a + " " + b + " ");
+        do
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            if (c<=sayac)
+            {
+                Console.Write(c + " ");
+            }
+        } while (c<=sayac);
+       
+        Console.ReadLine();
+
+    }
+}
+
+
